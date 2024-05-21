2033ab737a558d9ebbfc4f15e7eb39db3624777b
Aleksandr G <gerbl444@gmail.com>
2024-05-21 18:43:53 +0300

    add TailwindCSS

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,29 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 const TableRow = (props) => {
   return (
     <tr>
-    <th scope="row">{props.index + 1}</th>
-    <td>
-      <NavLink to={"/profile/" + props.id}>
-        {props.name} {props.lastname}
-      </NavLink>
-    </td>
-    <td>{props.email}</td>
-    <td>{props.id}</td>
-  </tr>
+      <th scope="row">{props.index + 1}</th>
+      <td>
+        <NavLink to={"/profile/" + props.id}>
+          {props.name} {props.lastname}
+        </NavLink>
+      </td>
+      <td>{props.email}</td>
+      <td>{props.id}</td>
+    </tr>
   );
 }
 
 export class Settings extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {userRow: []};
+    this.state = { userRow: [] };
   }
 
   componentDidMount() {
-  
+    this.props.function().then((users) => {
+      // console.log(users);
+      let usersCount = users.length;
+      let userRow = [];
+      for (let i = 0; i < usersCount; i++) {
+        userRow.push(<TableRow name={users[i].name} lastname={users[i].lastname} index={i} key={i} email={users[i].email} id={users[i].id} />);
+      }
+      this.setState({userRow: userRow})
+    });
   }
 
   render() {
@@ -44,30 +52,3 @@ export class Settings extends React.Component {
     )
   }
 }
-
-
-
-export const Settings = (props) => {
-  let users = props.function();
-  console.log(users);
-  let usersCount = Object.keys(users).length;
-  let userRow = [];
-  for (let i = 0; i < usersCount; i++) {
-    userRow.push(<TableRow name={users[i].name} lastname={users[i].lastname} index={i} key={i} email={users[i].email} id={users[i].id}/>);
-  }
-  return (
-    <div className="row">
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Имя и фамилия</th>
-            <th scope="col">Email</th>
-            <th scope="col">Id</th>
-          </tr>
-        </thead>
-        <tbody>{userRow}</tbody>
-      </table>
-    </div>
-  )
-};
